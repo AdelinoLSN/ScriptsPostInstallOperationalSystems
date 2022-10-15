e9700736d54f3cc257b9e87653e7d286dbb7f7ee
AdelinoLSN <adelinolsn@gmail.com>
2022-10-15 17:54:51 -0300

    Removed: OhMyZsh documentation

--- Oh My Zsh/install_and_config.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install wget git zsh
-zsh
-cd /home/adelino/
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
-rm -rf .zshrc && cp /home/adelino/Workspace/scripts_post_install_os/oh-my-zsh/.zshrc /home/adelino/zshrc
